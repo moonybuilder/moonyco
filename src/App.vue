dff2819086f684afe056bc9e5f22e71d0f2887af
moonybuilder <130939050+moonybuilder@users.noreply.github.com>
2025-09-29 17:52:41 +0000

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,9 @@
 <template>
   <div id="app">
-    <!-- Put any global nav/footer here if you want them on every page -->
-    <router-view />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: "App"
-};
+export default { name: 'App' }
 </script>
-
-<style>
-/* global app-level styles can go here (not scoped) */
-#app {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-/* keep the body background and fonts in page components or put global styles in main.css */
-</style>
