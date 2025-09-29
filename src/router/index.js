f58dab7bca3f8396d78b18db84dcb824ebf08a95
moonybuilder <130939050+moonybuilder@users.noreply.github.com>
2025-09-29 17:46:08 +0000

    updates homepage defaults

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from '@/App.vue'
+import HomePage from '@/components/HomePage.vue'
 import TheHotel from '@/components/TheHotel.vue'
 import BookNow from '@/components/BookNow.vue'
 import ReachUs from '@/components/ReachUs.vue'
@@ -12,7 +12,7 @@ export default new Router({
   mode: 'hash',
   base: '/moonyco/',
   routes: [
-    { path: '/', name: 'App', component: App },
+    { path: '/', name: 'HomePage', component: HomePage },
     { path: '/hotel', name: 'TheHotel', component: TheHotel },
     { path: '/contact', name: 'ReachUs', component: ReachUs },
     { path: '/login', name: 'LogIn', component: LogIn },
